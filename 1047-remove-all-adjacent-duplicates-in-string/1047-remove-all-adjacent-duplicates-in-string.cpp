4183cbc264f1f95c3122635b4843fb93708da9d0
saumya keshariya <104722509+Saumya-1008@users.noreply.github.com>
2024-03-22 21:57:02 +0530

    Time: 18 ms (65.45%), Space: 12.4 MB (50.81%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -6,6 +6,7 @@ public:
         while(in < s.length()){
             if(ans.length()>0 && ans[ans.length()-1]==s[in]){
              ans.pop_back();
+                
             }
             else{
                 ans.push_back(s[in]);
