e048d8cb1ab46e853f604af20e6da2f2975d8d5f
saumya keshariya <104722509+Saumya-1008@users.noreply.github.com>
2026-03-08 22:28:09 +0530

    Time: 4 ms (11.15%), Space: 71.7 MB (80.18%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        return maxSubArray(nums, 0, size(nums)-1);
+       int max=INT_MIN,sum=0,r;
+     //  if(nums)
+        for(int i=0;i<nums.size();i++){
+            r=sum+nums[i];
+             if(r>max)
+            max=r;
+            if(r>0)
+            sum=r;
+            else
+            sum=0;
+          //  if(sum>max)
+           // max=sum;
+        }
+        return max;
     }
-    int maxSubArray(vector<int>& A, int L, int R){
-        if(L > R) return INT_MIN;
-        int mid = (L + R) / 2, leftSum = 0, rightSum = 0;
-        // leftSum = max subarray sum in [L, mid-1] and starting from mid-1
-        for(int i = mid-1, curSum = 0; i >= L; i--)
-            curSum += A[i],
-            leftSum=max(leftSum, curSum);
-        // rightSum = max subarray sum in [mid+1, R] and starting from mid+1
-        for(int i = mid+1, curSum = 0; i <= R; i++)
-            curSum += A[i],
-            rightSum = max(rightSum, curSum);        
-		// return max of 3 cases 
-        return max({ maxSubArray(A, L, mid-1), maxSubArray(A, mid+1, R), leftSum + A[mid] + rightSum });
-    }	
 };
